017cd1848b81090216365ad6ae2495bbf2ae2318
Jose Domingo Peinado <jhosedo@gmail.com>
2014-02-09 11:38:25 +0100

    part03

--- polls/models.py
+++ polls/models.py
@@ -14,7 +14,6 @@ class Poll(models.Model):
 	was_published_recently.admin_order_field = 'pub_date'
 	was_published_recently.boolean = True
 	was_published_recently.short_description = 'Published recently?'
-	search_fields = ['question']
 
 class Choice(models.Model):
 	poll 		= models.ForeignKey(Poll)
